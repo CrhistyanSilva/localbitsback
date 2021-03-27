00a387fe78f47a654a70c777496050835e119c5f
Crhistyan Silva <cr.silper@gmail.com>
2021-03-27 12:38:15 -0300

    batch scripts for lbb

---
+++ batch/run_lbb_cifar10.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=lbb_cifar10
+#SBATCH --ntasks=1
+#SBATCH --mem=32G
+#SBATCH --time=6:00:00
+#SBATCH --partition=normal
+#SBATCH --qos=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=user@mail
+#SBATCH --output=%x_%j.out
+
+source /etc/profile.d/modules.sh
+source ~/anaconda/bin/activate
+conda activate lbb
+cd ~/proyecto_grado/lbb/localbitsback
+
+PYTHONPATH=./:compression/ans/build/ python scripts/run_compression.py --dataset cifar10 --mode val_only 
+
