beef203103680e16ef85f289937f560473f04089
Crhistyan Silva <cr.silper@gmail.com>
2021-04-15 18:58:21 -0300

    added conda variables for batch scripts

--- batch/run_lbb_imagenet64.sh
+++ batch/run_lbb_imagenet64.sh
@@ -11,7 +11,7 @@
 #SBATCH --output=%x_%j.out
 
 source /etc/profile.d/modules.sh
-source ~/anaconda/bin/activate
+source $CONDA_PYTHON_EXE
 conda activate lbb
 cd ~/proyecto_grado/lbb/localbitsback
 
